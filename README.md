ad01a15cd2e9fc10197d2683ef304d433141757e
Jimmy G <angelone55@hotmail.com>
2019-10-10 22:40:23 -0400

    added sample2 code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Java 101
+Java 101
+========
 Contains various applications to teach Java
+
+## Included Applications
+* Sample 1
+  * Simplest java application in the universe
+* Sample 2
+  * Processes a given file
+
+## References
+Below are some reference web sites
+* https://docs.oracle.com/javase/8/docs/api/
+* https://www.tutorialspoint.com/batch_script/index.htm
---
+++ sample2/src/dg/sample2/FileParser.java
@@ -0,0 +1,47 @@
+package dg.sample2;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Application to read each line a given file and process it.
+ */
+public class FileParser {
+
+  public static void main(String [] args) {
+    System.out.println("\n////////// SAMPLE 2 //////////");
+
+    FileParser parser = new FileParser();
+
+    if( args.length == 0 ) {
+      System.out.println("Filename missing as argument");
+    }
+    else {
+      parser.processFile( args[0] );
+    }
+
+    System.out.println("//////////////////////////////");
+  }
+
+  private void processFile( String fileName ) {
+    System.out.println("Parsing file: " + fileName);
+
+    List<String> lines;
+    try {
+      lines = Files.readAllLines(Paths.get(new URL("file:///f:\\dev\\java101\\sample2.csv").toURI()));
+      Iterator<String> it = lines.iterator();
+      System.out.println("Number of records: " + lines.size());
+
+      while( it.hasNext() ) {
+        System.out.println("Line: " + it.next());
+      }
+    }
+    catch (Exception e) {
+      System.out.println("Unable to read file: " + e);
+      e.printStackTrace();
+    }
+  }
+}
