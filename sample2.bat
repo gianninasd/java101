ca69ee302bdbefce64fc9c148bbfc099ce64307a
gianninasd <angelone55@hotmail.com>
2019-10-11 08:17:55 -0400

    updated classpath

--- sample2.bat
+++ sample2.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 REM Windows batch file to launch Java application
 
-java -cp f:\dev\java101\out\production\sample2 dg.sample2.FileParser %1
+java -cp out\production\sample2 dg.sample2.FileParser %1
