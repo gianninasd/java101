bb84d6c954a4397983427aa97d3dfd8890f90f60
Jimmy G <angelone55@hotmail.com>
2019-10-12 15:07:23 -0400

    added file record parsing

--- sample2/src/dg/sample2/FileParser.java
+++ sample2/src/dg/sample2/FileParser.java
@@ -13,6 +13,9 @@ public class FileParser {
 
   private static final String URL_FILE_PREFIX = "file:///";
 
+  /**
+   * Where application starts
+   */
   public static void main(String [] args) {
     System.out.println("\n////////// SAMPLE 2 //////////");
 
@@ -28,18 +31,32 @@ public class FileParser {
     System.out.println("//////////////////////////////");
   }
 
+  /**
+   * Reads the file provided and parses each record
+   */
   private void processFile( String fileName ) {
     System.out.println("Parsing file: " + fileName);
 
     List<String> lines;
+    String line;
+    PaymentRecord rec;
+
     try {
       lines = Files.readAllLines(Paths.get(new URL(URL_FILE_PREFIX + fileName).toURI()));
       Iterator<String> it = lines.iterator();
-      System.out.println("Number of records: " + lines.size());
+      System.out.println("Number of records: " + lines.size() + "\n");
 
       while( it.hasNext() ) {
-        System.out.println("Line: " + it.next());
+        line = it.next();
+        //System.out.println("Line: " + line);
+
+        if( line != null && line != "" && line.length() != 0 ) {
+          rec = PaymentRecord.parse( line );
+          System.out.println("rec: " + rec);
+        }
       }
+
+      System.out.println("\nTotal records parsed: " + PaymentRecord.totalCount);
     }
     catch (Exception e) {
       System.out.println("Unable to read file: " + e);
